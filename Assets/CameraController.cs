c540256216f6b3e95c6e6261c4f4c2fac77ae4cc
Nadine Mansour <nadine.mansour94@gmail.com>
2016-12-01 12:07:52 +0200

    Adding the badic character animations.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
+	void Update ()
+	{
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			// Read the mouse input axis
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+			//transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			//transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotationY, Vector3.right);
+			//transform.localRotation = originalRotation * yQuaternion;
+		}
+		//transform.LookAt(Camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.nearClipPlane)), Vector3.up);
+	}
+	void Start ()
+	{
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle <= -360F)
+			angle += 360F;
+		if (angle >= 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+
+	// The target we are following
+	[SerializeField]
+	private Transform target;
+	// The distance in the x-z plane to the target
+	[SerializeField]
+	private float distance = 10.0f;
+	// the height we want the camera to be above the target
+	[SerializeField]
+	private float height = 5.0f;
+	[SerializeField]
+	private float rotationDamping;
+	[SerializeField]
+	private float heightDamping;
+	void LateUpdate()
+	{
+			// Early out if we don't have a target
+			if (!target)
+				return;
+
+			// Calculate the current rotation angles
+			var wantedRotationAngle = target.eulerAngles.y;
+			var wantedHeight = target.position.y + height;
+
+			var currentRotationAngle = transform.eulerAngles.y;
+			var currentHeight = transform.position.y;
+
+			// Damp the rotation around the y-axis
+			currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+			// Damp the height
+			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+			// Convert the angle into a rotation
+			var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+			// Set the position of the camera on the x-z plane to:
+			// distance meters behind the target
+			transform.position = target.position;
+			transform.position -= currentRotation * Vector3.forward * distance;
+
+			// Set the height of the camera
+			float xPosition = transform.position.x;
+			float zPosition = transform.position.z;
+			
+			transform.position = new Vector3(xPosition ,wantedHeight , zPosition);
+
+			// Always look at the target
+			//transform.LookAt(new Vector3(target.position.x,2,target.position.z));
+			
+	}
+
+	
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Animator animator;
+	private float speed = 3.0f;
+	private bool walkForward, walkBackward, suspendMove, jump, crawlForward;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		walkForward = false;
+		walkBackward = false;
+		crawlForward = false;
+		jump = false;
+
+		if (Input.GetKey("w")){
+			//Walk forward
+			walkForward = true;
+			transform.Translate (0, 0, Input.GetAxis("Vertical")*2*Time.deltaTime);
+		}
+		
+		if (Input.GetKey("s")){
+			//Walk backward
+			walkForward = true;
+			walkBackward = true;
+			transform.Translate (0, 0, Input.GetAxis("Vertical")*2*Time.deltaTime);
+		}
+
+		if (Input.GetKey("d")){
+			//rotate clockwise
+			transform.Rotate(Vector3.up * 50 * Time.deltaTime);
+			walkForward = true;
+		}
+		
+
+		if (Input.GetKey("a")){
+			//rotate anti-clockwise
+			transform.Rotate(Vector3.up * -50 * Time.deltaTime);
+			walkForward = true;
+		}
+
+		if (Input.GetKey("left shift")){
+			crawlForward = true;
+			transform.Translate (0, 0, Time.deltaTime);
+		}
+
+		if (Input.GetKey("space")){
+			//jump
+			jump = true;
+		}
+	
+		animator.SetBool("walkForward", walkForward );
+		animator.SetBool("crawlForward", crawlForward );
+		animator.SetBool("jump", jump);
+
+		if(walkBackward){
+			animator.SetFloat("walkingDirection", -1.0f );
+		} else{
+			animator.SetFloat("walkingDirection", 1.0f );
+		}
+	}
+}
