0f22249b6ce0e6a6182c9048189e5cd4e5f89dfe
mohamadbadr <m.badr94@gmail.com>
2016-12-16 03:03:48 +0200

    enemy distraction version 1

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,10 +9,14 @@ public class EnemyController : MonoBehaviour {
 
     bool walk;
     bool stop;
-    bool rotate;
+    bool rotate;    
+    bool distractedRunning;
     bool positveDirection;
 
-    string stage;  
+    public bool distracted;
+    public Vector3 distractionPosition;
+
+    public string stage;  
 
     float timer;
 
@@ -23,11 +27,13 @@ public class EnemyController : MonoBehaviour {
         stop = false;
         rotate = false;
         positveDirection = false;
+        distracted = false;
         timer = 4;
         stage = "walk";        
         enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");    
+        distractionPosition = new Vector3(0,0,0);
         
 	}
 	
@@ -38,27 +44,30 @@ public class EnemyController : MonoBehaviour {
         enemy.SetDestination(target.position);
         anim.SetBool("stop", stop);
         anim.SetBool("walk", walk);
-        anim.SetBool("rotate", rotate);                        
+        anim.SetBool("rotate", rotate);
+        anim.SetBool("running", distractedRunning);
+        anim.SetBool("distracted", distracted);
     }
 
     void motionController()
     {
+        Debug.Log(stage);
         if (stage == "idle")
-        {           
+        {
             timer -= Time.deltaTime;
             stop = true;
             walk = false;
             rotate = false;
 
-            if(timer < 0)
+            if (timer < 0)
             {
                 timer = 1.5f;
-                stage = "rotate"; 
+                stage = "rotate";
             }
-            
+
         }
-        else if(stage == "rotate")
-        {           
+        else if (stage == "rotate")
+        {
             rotate = true;
             walk = false;
             stop = false;
@@ -83,25 +92,105 @@ public class EnemyController : MonoBehaviour {
                     positveDirection = true;
                 }
             }
-
-            
-
-            //check if rotation animation has ended
-                //do somthing
-        }
+        }   
         else if (stage == "walk")
-        {         
+        {
             walk = true;
             stop = false;
             rotate = false;
             if (transform.position.x == target.position.x && transform.position.z == target.position.z)
-            {             
+            {
                 stage = "idle";
                 stop = true;
                 rotate = false;
                 walk = false;
             }
         }
+       if(stage == "distracted")
+        {
+            walk = false;
+            stop = true;
+            rotate = false;
+            distractedRunning = false;        
+            target.position = transform.position;
+            transform.LookAt(distractionPosition);            
+            stage = "distracted running";
+        }
+        else if(stage == "distracted running")
+        {
+            enemy.speed = 2;
+            distractedRunning = true;
+            walk = false;
+            stop = false;
+            rotate = false;
+            target.position = distractionPosition;
+
+            if (transform.position.x == target.position.x && transform.position.z == target.position.z)
+            {                               
+                stage = "distracted idle";
+                timer = 4;                
+                enemy.speed = 1;
+            }
+
+        }
+       else if (stage == "distracted idle")
+        {
+            timer -= Time.deltaTime;
+            stop = true;
+            rotate = false;
+            walk = false;            
+            distractedRunning = false;
+
+            if (timer < 0)
+            {
+                target.position = new Vector3(5, 0, 4);
+                walk = true;
+                stop = false;
+                distractedRunning = false;
+                rotate = false;                
+                stage = "walking to path";                
+            }            
+        }
+       else if(stage == "walking to path")
+        {
+            /*if(transform.position.x == target.position.x && transform.position.z == target.position.z)
+            {                
+                Vector3 Z4 = new Vector3(5, 0, 4);
+                Vector3 Z6 = new Vector3(5, 0, -6);
+                float angle1 = Vector3.Angle(Z4, transform.forward);
+                float angle2 = Vector3.Angle(Z6, transform.forward);                
+                if (angle1 <= angle2)
+                {
+                    target.position = new Vector3(5, 0, 4);
+                }
+                else
+                {
+                    target.position = new Vector3(5, 0, -6);
+                }
+                distracted = false;
+                stage = "walk";
+
+
+            }            */
+
+            if(positveDirection)
+            {
+                target.position = new Vector3(5, 0, 8);
+                positveDirection = false;
+                stage = "walk";
+                timer = 4;
+                distracted = false;
+            }
+            else
+            {
+                target.position = new Vector3(5, 0, -2);
+                positveDirection = true;
+                distracted = false;
+                stage = "walk";
+                timer = 4;
+            }
+        }
+        
     }
 
     void FieldOfView()
